d8e198b97c8d5b32d813bf35769a8ceb19a075b3
saracsilva <saracarolinasilva@gmail.com>
2022-11-15 18:01:06 +0100

    iteration_3 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import contactsJSON from "./contacts.json";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5));
-
+  const firstFive = contactsJSON.slice(0, 5);
+  const [contacts, setContacts] = useState(firstFive);
+  contactsJSON = contactsJSON.slice(5);
+  function addRandomContact() {
+    let randomIndex = Math.floor(Math.random() * contactsJSON.length);
+    let randomContact = contactsJSON[randomIndex];
+    setContacts([...contacts, randomContact]);
+    contactsJSON.splice(randomIndex, 1);
+  }
   return (
     <div className="App">
+      <button onClick={addRandomContact}>Add Random Contact</button>
       <table>
         <thead>
           <tr>
@@ -16,14 +23,16 @@ function App() {
             <th>Name</th>
 
             <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
           </tr>
         </thead>
         <tbody>
           {contacts.map((contact) => {
             return (
-              <tr>
+              <tr key={contact.id}>
                 <td>
-                  <img src={contact.pictureUrl} width="80px" />
+                  <img src={contact.pictureUrl} width="80px" alt="Yes" />
                 </td>
                 <td>
                   <h5>{contact.name} </h5>
@@ -31,6 +40,17 @@ function App() {
                 <td>
                   <h5>{contact.popularity.toFixed(2)} </h5>
                 </td>
+                {contact.wonOscar === true && (
+                  <td>
+                    <h5>🏆</h5>
+                  </td>
+                )}
+
+                {contact.wonEmmy === true && (
+                  <td>
+                    <h5>🏆</h5>
+                  </td>
+                )}
               </tr>
             );
           })}
